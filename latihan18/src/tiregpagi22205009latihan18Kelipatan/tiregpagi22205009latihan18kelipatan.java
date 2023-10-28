1c456a515f0f10b8aff6f5c0ce0117bb52a0fe6d
Hudri07 <148939802+Hudri07@users.noreply.github.com>
2023-10-28 11:42:55 +0700

    Add files via upload

---
+++ latihan18/src/tiregpagi22205009latihan18Kelipatan/tiregpagi22205009latihan18kelipatan.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiregpagi22205009latihan18Kelipatan;
+
+/**
+ *
+ * @author Axioo
+ *  NAMA     : Asmalik Hudri
+ * Prodi    : Teknik Informatika
+ * NIM      : 22205009
+ * Deskripsi Program    : Program ini berisi program untuk menampilkan
+ * bilangan kelipatan 3,5 dimulai dari 3,5 sampai 35
+ */
+public class tiregpagi22205009latihan18kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for(double i=3.5; i<= 35; i+= 3.5){
+            System.out.println(i);
+        }
+     
+    }
+}
+
+    
